f415a37977ba0340df7ad1c3b48b13195955b4f6
JuanPabe <Bjuampihotmailcom@gmail.com>
2026-05-01 15:41:06 -0300

    feat(tickets): actualizar controlador de tickets

--- server/src/tickets/tickets.controller.ts
+++ server/src/tickets/tickets.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { TicketsService } from './tickets.service';
 
 @Controller('tickets')
-export class TicketsController { }
+export class TicketsController {
+    constructor(private readonly ticketsService: TicketsService) {}
+
+    @Post('comprar')
+    async comprarTickets(
+        @Body('partidoId') partidoId: number,
+        @Body('usuarioId') usuarioId: string,
+        @Body('cantidad') cantidad: number
+    ) {
+        return await this.ticketsService.procesarCompra(partidoId, usuarioId, cantidad);
+    }
+}
