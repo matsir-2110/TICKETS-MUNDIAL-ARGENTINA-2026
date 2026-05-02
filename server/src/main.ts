87d7591e44f341bff46264dd376d12699246211e
Maximo <maximors@gmail.com>
2026-05-01 21:14:40 -0300

    Agregado de .env, dtos y cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Habilitar CORS para permitir peticiones desde el frontend
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  });
   // El backend escuchará en el puerto 3000
-  await app.listen(3000);
+  await app.listen(3001);
   console.log('Servidor corriendo en: http://localhost:3000');
 }
 bootstrap();
