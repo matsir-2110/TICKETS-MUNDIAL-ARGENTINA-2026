db422cbf811fb5125bcef6cc1d9ca1ef35439109
Maximo <maximors@gmail.com>
2026-05-04 18:49:40 -0300

    Fix SupabaseKey de client

--- client/src/utils/supabase/client.ts
+++ client/src/utils/supabase/client.ts
@@ -2,6 +2,6 @@ import { createClient as supabaseCreateClient } from '@supabase/supabase-js';
 
 export function createClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
   return supabaseCreateClient(supabaseUrl, supabaseAnonKey);
 }
