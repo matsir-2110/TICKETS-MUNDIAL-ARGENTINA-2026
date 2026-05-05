d17524c931f8aec49580c4db26d190fc3e08bd27
tomy-sego <juantomasegovia05@gmail.com>
2026-05-05 09:49:09 -0300

    arreglo algunos errores

--- server/src/tickets/observer/audit-log.observer.ts
+++ server/src/tickets/observer/audit-log.observer.ts
@@ -1,10 +1,10 @@
 @Injectable()
 export class AuditLogObserver implements TicketEventObserver {
   async onPagoConfirmado(pedidoId: string, usuarioId: string, monto: number) {
-    console.log(Audit: pago confirmado - pedido ${pedidoId}, $${monto});
+    console.log('Audit: pago confirmado - pedido ${pedidoId}, $${monto}');
   }
 
   async onPedidoCreado(pedidoId: string, usuarioId: string) {
-    console.log(Audit: nuevo pedido ${pedidoId} por usuario ${usuarioId});
+    console.log('Audit: nuevo pedido ${pedidoId} por usuario ${usuarioId}');
   }
 }
